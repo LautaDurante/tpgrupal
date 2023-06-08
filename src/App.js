79678e24bc1a957351aa229c722ab5950536ecb6
LautaDurante <durantelautarop@gmail.com>
2023-06-08 16:44:10 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import logo from './logo.svg';
+
+import React, { useRef } from 'react';
 import './App.css';
+import * as tf from '@tensorflow/tfjs';
+import * as posenet from '@tensorflow-models/posenet';
+import Webcam from 'react-webcam';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  //cargar posenet
+  const runPosenet = async () => {
+    const net = await posenet.load({
+      inputResolution: { width: 640, height:480},
+      scale: 0.5
+    });
+    setInterval(() =>{
+      detect(net);  
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if(
+    typeof webcamRef.current !== 'undefined' && 
+    webcamRef.current !== null && 
+    webcamRef.current.video.readyState ===4 
+    ) {
+      //obtener propiedades de video
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      //establecer ancho de video
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      //hacer las detecciones
+      const pose = await net.estimateSinglePose(video);
+      console.log(pose);
+    }  
+  };
+
+  runPosenet();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+        ref={webcamRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zindex: 9,
+          width: 640,
+          height: 480
+        }}
+        />
+
+        <canvas 
+        ref={canvasRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zindex: 9,
+          width: 640,
+          height: 480
+        }} />
       </header>
     </div>
   );
